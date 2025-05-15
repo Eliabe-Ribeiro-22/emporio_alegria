d1d71d7a9ea63ef48d144df1cd78193d0ac2ddb1
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2025-05-15 11:02:28 -0300

    html - form produtos

--- paginas/produtos.php
+++ paginas/produtos.php
@@ -13,8 +13,23 @@
 <main>
 	<h1>Cadastrar Produto</h1>
 	<form>
-		<label></label>
-		<input type="text">
+		<label>Código Produto:</label>
+		<input type="text"disabled name="codigo" id="codigo">
+	
+		<label>Nome Produto:</label>
+		<input type="text"  name="nome-produto" id="nome-produto">
+	
+		<label>Estoque Mínimo Produto:</label>
+		<input type="number" name="estoque-minimo" id="estoque-minimo">
+	
+		<label>Estoque Máximo Produto:</label>
+		<input type="number" name="estoque-maximo" id="estoque-maximo">
+	
+		<label>Valor Unitário Produto:</label>
+		<input type="text" name="valor-unitario" id="valor-unitario">
+	
+		<label>Quantidade Produto:</label>
+		<input type="number" name="quantidade" id="quantidade">
 	
 	</form>
 <a href="../index.php">Voltar ao inicio</a>
