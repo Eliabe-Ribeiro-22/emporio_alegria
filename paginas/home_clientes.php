91750c27873c32f3c22bc580644944cfb78376a2
Eliabe Ribeiro Mota <100526198+Eliabe-Ribeiro-22@users.noreply.github.com>
2025-05-21 15:17:32 -0300

    sql delete clientes

--- paginas/home_clientes.php
+++ paginas/home_clientes.php
@@ -2,4 +2,8 @@
 <a href="../index.php">Voltar ao início</a>
 
 CLIENTES CADASTRADOS 
-trash: 🗑
+trash: 🗑 
+//deseja realmente excluir o cliente? SIM OU Não 
+// SQL DELETE FROM CLIENTES WHERE ID = id_produto_selected=[$key]
+// se erro, informar mensagem ao usuário
+// cliente deletado com sucesso
