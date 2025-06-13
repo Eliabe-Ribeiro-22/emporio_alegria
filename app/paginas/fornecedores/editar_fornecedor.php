4ae336ebd95ccab0b2abe4d80905601c9595c432
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2025-06-13 17:07:55 -0300

    html: alterar dados de fornecedores, clientes, produtos

---
+++ app/paginas/fornecedores/editar_fornecedor.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Fornecedor</title>
+</head>
+<body>
+	<header>
+	cabeçalho
+	</header>
+
+	<main>
+		<h1>Cadastre um fornecedor</h1>
+		<a href="../../../index.php">Voltar ao inicio</a>
+		<form action="./atualizar_fornecedor.php" method="POST" onsubmit="return valida_fornecedores()">
+			<label>Código:</label>
+			<input type="text"disabled value="0" name="codigo-fornecedor" id="codigo_fornecedor">
+
+			<label>Nome:</label>
+			<input type="text" name="nome_fornecedor" id="nome-fornecedor">
+
+			<label>Telefone:</label>
+			<input type="text" name="telefone_fornecedor" id="telefone-fornecedor">
+
+			<label>Email:</label>
+			<input type="email" name="email_fornecedor" id="email-fornecedor">
+
+			
+			<input type="submit" value="atualizar" name="atualizar">
+		</form>
+		<script type="text/javascript" src="../../assets/js/fornecedores.js"></script>
+	</main>
+</body>
+</html>
