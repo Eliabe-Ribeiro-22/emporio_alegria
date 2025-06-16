a27b7b79e340873f891a21f295994aa328833f88
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2025-06-16 19:05:10 -0300

    php: valores passados para html form cliente

--- app/paginas/clientes/editar_cliente.php
+++ app/paginas/clientes/editar_cliente.php
@@ -14,17 +14,27 @@
 		<?php 
 		$id_cliente = $_GET['id'];
 		echo "id é: " .  $id_cliente;	
-		//sql = select * from clientes where id_cliente = $id_cliente	
 		
 		require_once "../../funcoes_php/pesquisar_cliente.php";
 		$cliente = pesquisa_cliente($id_cliente);
+		
 		foreach ($cliente as $key => $value) {
-			echo "ID DO CLIENTE É: " .  $value["ID_CLIENTE"];
-			echo "Nome do Cliente é: ". $value["NOME_CLIENTE"];
-			echo "ENDERECO DO CLIENTE É: " . $value["ENDERECO_CLIENTE"];
-			echo "TELEFONE DO CLIENTE É: " . $value["TELEFONE_CLIENTE"];
-			echo "EMAIL DO CLIENTE É: " . $value["EMAIL_CLIENTE"];
-			echo "CIDADE DO CLIENTE É: " . $value["CIDADE_CLIENTE"];
+			
+			// armazenando dados em variaveis
+			$id_cliente = $value["ID_CLIENTE"];
+			$nome_cliente = $value["NOME_CLIENTE"];
+			$endereco_cliente = $value["ENDERECO_CLIENTE"];
+			$telefone_cliente = $value["TELEFONE_CLIENTE"];
+			$email_cliente = $value["EMAIL_CLIENTE"];
+			$cidade_cliente = $value["CIDADE_CLIENTE"];
+			
+			// printando na tela
+			echo "ID DO CLIENTE É: " . $id_cliente;
+			echo "Nome do Cliente é: ". $nome_cliente;
+			echo "ENDERECO DO CLIENTE É: " . $endereco_cliente;
+			echo "TELEFONE DO CLIENTE É: " . $telefone_cliente;
+			echo "EMAIL DO CLIENTE É: " . $email_cliente;
+			echo "CIDADE DO CLIENTE É: " . $cidade_cliente;
 		}
 		 
 		?>
@@ -32,22 +42,22 @@
 		<a href="../../../index.php">Voltar ao inicio</a>
 		<form action="./salvar.php" method="POST" onsubmit="return valida_clientes()">
 			<label>Código:</label>
-			<input type="text"disabled value="0" name="codigo_cliente" id="codigo-cliente">
+			<input type="text"disabled value="<?php echo $id_cliente ?>" name="codigo_cliente" id="codigo-cliente">
 
 			<label>Nome:</label>
-			<input type="text" name="nome_cliente" id="nome-cliente">
+			<input type="text" name="nome_cliente" id="nome-cliente" value="<?php echo $nome_cliente ?>">
 
 			<label>Endereço:</label>
-			<input type="text" name="endereco_cliente" id="endereco-cliente">
+			<input type="text" name="endereco_cliente" id="endereco-cliente" value="<?php echo $endereco_cliente ?>">
 
 			<label>Telefone:</label>
-			<input type="text" name="telefone_cliente" id="telefone-cliente">
+			<input type="text" name="telefone_cliente" id="telefone-cliente" value="<?php echo $telefone_cliente ?>">
 
 			<label>Email:</label>
-			<input type="email" name="email_cliente" id="email-cliente">
+			<input type="email" name="email_cliente" id="email-cliente" value="<?php echo $email_cliente ?>">
 
 			<label>Cidade:</label>
-			<input type="text" name="cidade_cliente" id="cidade-cliente">
+			<input type="text" name="cidade_cliente" id="cidade-cliente" value="<?php echo $cidade_cliente ?>">
 	
 			<input type="submit" value="editar" name="editar">
 		</form>
